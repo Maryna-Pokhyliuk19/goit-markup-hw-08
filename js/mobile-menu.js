396f7714ddd608bf84f2a41e5c2bee057625f235
Maryna Pokhyliuk <marinapohiluk19@gmail.com>
2022-05-29 01:11:17 +0300

    корректирует замечания

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  const bodyMobile = document.querySelector('[body-mobile]');
+  const bodyMobile = document.querySelector('body');
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     menuBtnRef.classList.toggle('is-open');
